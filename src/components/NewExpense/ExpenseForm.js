cc21a24c5348b46f5dd2ce18beea07748998b5d4
MennaTalhHossamAlden <76497207+MennaTalhHossamAlden@users.noreply.github.com>
2023-07-09 22:19:47 +0300

    updating state that depend on another state

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -3,32 +3,38 @@ import { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-//   const [enteredTitle, setEnteredTitle] = useState("");
-//   const [enteredAmount, setEnteredAmount] = useState("");
-//   const [enteredDate, setEnteredDate] = useState("");
-    const [userInput, setUserInput] = useState({
-        enteredTitle: '',
-        enteredAmount: '',
-        enteredDate: ''
-    });
+    const [enteredTitle, setEnteredTitle] = useState("");
+    const [enteredAmount, setEnteredAmount] = useState("");
+    const [enteredDate, setEnteredDate] = useState("");
+    //   const [userInput, setUserInput] = useState({
+    //     enteredTitle: "",
+    //     enteredAmount: "",
+    //     enteredDate: "",
+    //   });
 
   const titleChangeHandler = (event) => {
-    setUserInput({
-        ...userInput,
-        enteredTitle: event.target.value,
-    });
+    setEnteredTitle(event.target.value);
+    // setUserInput({
+    //     ...userInput,
+    //     enteredTitle: event.target.value,
+    // });
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredTitle: event.target.value };
+    // });
   };
   const amountChangeHandler = (event) => {
-    setUserInput({
-        ...userInput,
-        enteredAmount: event.target.value,
-    });
+    setEnteredAmount(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredAmount: event.target.value,
+    // });
   };
   const dateChangeHandler = (event) => {
-    setUserInput({
-        ...userInput,
-        enteredDate: event.target.value,
-    });
+    setEnteredDate(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredDate: event.target.value,
+    // });
   };
   return (
     <form>
